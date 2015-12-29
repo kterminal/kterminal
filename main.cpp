15c07df4494241791ab7da078e98b011b04569a2
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2015-12-29 09:48:51 +0100

    button inside container which is simply: qwidget

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
-#include <QtWidgets/QApplication>
-#include <QtWidgets/QLabel>
+#include <QApplication>
+//#include "mainwindow.h"
+#include <QPushButton>
 
-int main(int argc, char *argv[])
+int main (int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QLabel *label = new QLabel("Hello Qt!");
-    label->show();
+    QWidget window;
+    window.setFixedSize(100, 50);
+    QPushButton *button = new QPushButton("yo", &window);
+    button->setGeometry(10, 10, 80, 30);
+    window.show();
     return app.exec();
 }
